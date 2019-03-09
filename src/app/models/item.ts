8f4e69699f524f6ce678e55cbf56bc6a5c85e113
alainlou <loual@utschools.ca>
2019-03-09 15:13:53 -0500

    integrated google maps

--- src/app/models/item.ts
+++ src/app/models/item.ts
@@ -2,11 +2,13 @@ export interface Item {
     title: string;
     type: string;
     description: string;
+    img: any;
     location: string;
     available: boolean;
 }
 
 export const ITEMS: Item [] = [
-    {title: 'Lawnmower', type: 'garden tool', description: 'A lawnmower', location: 'here', available: true},
-    {title: 'Lawnmower', type: 'garden tool', description: 'A lawnmower', location: 'here', available: true}
+    {title: 'Lawnmower', type: 'garden tool', description: 'A lawnmower', img: '', location: 'here', available: true},
+    {title: 'Shovel', type: 'garden tool', description: 'A shovel', img: '', location: 'here', available: true},
+    {title: 'Car', type: 'transportation', description: 'A 2006 Honda Civic', img: '', location: 'here', available: true}
 ];
